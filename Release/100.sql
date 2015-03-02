af65d96d1846f5e2259fee7b0e70fcd82f262131
Rajiv Navada <rajiv@appcove.com>
2015-03-02 07:53:35 -0500

    Added release table for creating initial version table for DocStruct

---
+++ Release/100.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION
+
+CREATE TABLE "AWS"."Release" (
+    "Version" varchar(32) NOT NULL COLLATE "default"
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "AWS"."Release" OWNER TO "TMTManage";
+
+INSERT INTO "AWS"."Release" ("Version") VALUES ('1.0.0');
+
+END
+
